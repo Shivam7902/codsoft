8a12a745e5a6b3a1c3902dbf90e0d2485d29d158
Shivam7902 <147232207+Shivam7902@users.noreply.github.com>
2023-10-07 12:49:40 +0530

    Update password-generate.py

--- password-generate.py
+++ password-generate.py
@@ -2,27 +2,25 @@ import random
 import string
 
 def generate_password(length):
-    # Define character sets for password complexity
     lowercase_letters = string.ascii_lowercase
     uppercase_letters = string.ascii_uppercase
     digits = string.digits
     special_characters = string.punctuation
 
-    # Combine character sets based on desired complexity
+    
     all_characters = lowercase_letters + uppercase_letters + digits + special_characters
 
-    # Check if the length is valid
+    
     if length < 4:
         print("Password length should be at least 4 characters.")
         return
 
-    # Generate a password by randomly selecting characters
+  
     password = random.choices(all_characters, k=length)
 
-    # Shuffle the characters to make it more random
+    
     random.shuffle(password)
 
-    # Convert the list of characters to a string
     password = ''.join(password)
 
     return password
